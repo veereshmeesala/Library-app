77670ebd0c09f65c5b936475002adca99f99ec8b
Veeresh Meesala <veereshmeesala@gmail.com>
2016-07-27 17:47:23 +0100

    getting one book with id(single item )

--- app.js
+++ app.js
@@ -22,6 +22,16 @@ bookRouter.route('/books')
 		  		})
 			});
 
+bookRouter.route('/books/:bookId')
+			.get(function(req, res){
+				Book.findById(req.params.bookId, function(err, book){
+					if(err)
+						res.status(500).send(err);
+					else
+						res.json(book);
+				});
+			});
+
 app.use('/api', bookRouter);
 
 app.get('/', function(req, res){
